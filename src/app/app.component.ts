ebccf3cd42fd4d0650e7b86654ebbd743afb7407
Lorenzo Massaro <massarolore05@gmail.com>
2024-06-26 23:15:08 +0200

    added csss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CsvConversionService } from './services/csv-conversion.service';
 
 @Component({
   selector: 'app-root',
@@ -8,8 +9,55 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  mainQuestion="cosa stavi cercando?"
+
+export class AppComponent implements OnInit {
+ 
+  constructor(
+    private csvService: CsvConversionService,
+    ) 
+    {}
+  
+  mainQuestion="Guida all aquisto"
+  secondQuestion="cosa stavi cercando?"
   title = 'product-guide';
   suggestedProducts=[{},{},{},{},{}]
+  products: any[]=[]
+  complete_categories_json:any={}
+
+  ngOnInit(): void {
+    console.log("kllkj")
+    const csvContent = `name,age,city
+    John,25,New York
+    Jane,30,San Francisco`;
+
+    let catalog=this.csvService.getMagentoCatalog().then((csv_text)=>{
+      this.csvService.convertCsvToJson(csv_text).then((res)=>{
+        console.log(res)
+        this.extract_categories(res)
+      })
+    })
+  }
+  extract_categories(json_catalog:any[]):any{
+    json_catalog.forEach((product:any)=>{
+      if(product.category != undefined){
+        let categories_from_product=product.category.split("/")
+        let current_obj=this.complete_categories_json
+        for(let i=0;i<categories_from_product.length;i++){
+          if(!current_obj[categories_from_product[i]]){
+            current_obj[categories_from_product[i]]={}
+            current_obj=current_obj[categories_from_product[i]]
+          }else{
+            current_obj=current_obj[categories_from_product[i]]
+          }
+        }
+      }
+      
+    })
+    console.log(this.complete_categories_json)
+
+  }
+
+
+
+
 }
